fef530780332b45fb162bc33905e57c04af48232
jd6 <jdev6@protonmail.com>
2017-08-24 11:40:05 +0200

    qwefgqweg

--- test.s
+++ test.s
@@ -4,7 +4,6 @@ jmp %start
 
 ;varables
 x: .db 0 ;counter
-y: .db 0 ;TEMP variabler for jz
 
 ;entry point
 start: nop
@@ -15,13 +14,12 @@ add
 sta %x
 
 ldi %x ;x -> a
-sta %y ;JZ uses A..... put it in termporary var
 ldbd 1 
 and ;A = x & 1
 
 sub ;A = A-1
 jz %start ;JUMPs if A&1 is 1 (means odd)
-ldi %y
+ldi %x
 out
 
 jmp %start
